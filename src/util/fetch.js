d79729deb993a2ce4cdfce432180e627c1aaae26
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2023-11-29 19:58:32 +0100

    remove old code

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -8,9 +8,7 @@ const getCoffees = async (url = '/api/') => {
         // const response = await fetch(url, { mode: 'no-cors' });
         // const response = await fetch(url);
         const response = await fetch('https://nodejs-database.onrender.com/api/v1/coffees/', {
-            mode: 'cors',
             method: 'GET',
-
           });
         if (response && !response.ok) {
             const error = response.status;
