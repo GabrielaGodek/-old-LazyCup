362c1fbaf0a1006d1f0223d30c9a6df05f69977d
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2023-11-29 16:57:27 +0100

    add credentials

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -7,7 +7,9 @@ const getCoffees = async (url = '/api/') => {
     try {
         // const response = await fetch(url, { mode: 'no-cors' });
         // const response = await fetch(url);
-        const response = await fetch('https://nodejs-database.onrender.com/api/v1/coffees/');
+        const response = await fetch('https://nodejs-database.onrender.com/api/v1/coffees/', {
+            credentials: 'include',
+          });
         if (response && !response.ok) {
             const error = response.status;
             const details = await response.text(); 
