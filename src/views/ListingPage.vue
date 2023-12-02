e4e6265e10876760a4f4dc4d4a0e7b9f0866f5d8
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2023-12-02 10:50:07 +0100

    add loader

--- src/views/ListingPage.vue
+++ src/views/ListingPage.vue
@@ -22,7 +22,8 @@ export default {
     });
 
     watch(coffees, () => {
-      loading.value = coffees.value.length > 0 ? false : true;
+      // loading.value = coffees.value.length > 0 ? false : true;
+      loading.value = true
     });
 
     return {
@@ -42,13 +43,47 @@ export default {
         <ProductTile :coffee="item" />
       </div>
     </template>
-    <div v-else>
-      <p>Loading...</p>
+    <div class="loading_wrapper" v-else>
+      <div class="loader"></div>
     </div>
   </section>
 </template>
 
 <style scoped lang="scss">
+.loader, .loader:before, .loader:after {
+  border-radius: 50%;
+  width: 2.5em;
+  height: 2.5em;
+  animation-fill-mode: both;
+  animation: bblFadInOut 1.8s infinite ease-in-out;
+}
+.loader {
+  color: #4f483e;
+  font-size: 7px;
+  position: relative;
+  text-indent: -9999em;
+  transform: translateZ(0);
+  animation-delay: -0.16s;
+}
+.loader:before,
+.loader:after {
+  content: '';
+  position: absolute;
+  top: 0;
+}
+.loader:before {
+  left: -3.5em;
+  animation-delay: -0.32s;
+}
+.loader:after {
+  left: 3.5em;
+}
+
+@keyframes bblFadInOut {
+  0%, 80%, 100% { box-shadow: 0 2.5em 0 -1.3em }
+  40% { box-shadow: 0 2.5em 0 0 }
+}
+    
 .wrapper.listing:has(.empty_res) {
   display: block;
   text-align: center;
