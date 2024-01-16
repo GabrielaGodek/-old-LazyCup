80bb0d2198cee5d4d5fd7e3d3474c34ccd04b7f2
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2024-01-16 20:57:02 +0100

    update fetch to abort

--- src/includes/fetch.js
+++ src/includes/fetch.js
@@ -1,36 +1,37 @@
-
 export const getCoffees = async (url = '/api/') => {
     let coffeesData = null
-    let controller = new AbortController()
+    const controller = new AbortController();
+
     try {
-        setTimeout(() => controller.abort(), 1000)
-        const response = await fetch(url, {
-            method: 'GET',
-            signal: controller.signal
-        });
+        const timeoutId = setTimeout(() => controller.abort(), 2000);
+
+        const response = await fetch(url, { signal: controller.signal });
+
+        clearTimeout(timeoutId);
+
         if (response && !response.ok) {
             const error = response.status;
             const details = await response.text();
             console.error(`HTTP error! Status: ${error}. Details: ${details}`);
             throw new Error(`HTTP error! Status: ${error}. Details: ${details}`);
         }
-        coffeesData = (await response.json()).coffees
+
+        coffeesData = (await response.json()).coffees;
     } catch (error) {
-        if (error.name == 'AbortError') {
+        if (error.name === 'AbortError') {
             try {
                 const backupResponse = await fetch('/backup/db.json');
                 coffeesData = (await backupResponse.json());
-                console.log(coffeesData)
+                console.log(coffeesData);
             } catch (backupError) {
                 console.error('Backup fetch error ❌', backupError);
                 throw backupError;
             }
+        } else {
+            console.error('Fetch error ❌', error);
+            throw error;
         }
-        console.error('Fetch error ❌', error);
-        throw error;
     }
 
-    return coffeesData ;
+    return coffeesData;
 };
-
-
